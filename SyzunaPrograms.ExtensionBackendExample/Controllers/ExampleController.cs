4bbb1f01f804d4c731723882d021ff9938fe9d1d
Syzuna <syzuna@t-teske.de>
2021-03-15 01:38:13 +0100

    Add project files.

---
+++ SyzunaPrograms.ExtensionBackendExample/Controllers/ExampleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SyzunaPrograms.ExtensionBackendExample.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("[controller")]
+    // All routes in this controller need to be authenticated with the Bearer Authentication Scheme
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet]
+        // We do not override the controller scope authentication settings here because we want everybody with a valid JWT access this endpoint
+        public ActionResult<string> ForEveryViewer()
+        {
+            return "Hello World! Welcome to the root example endpoint";
+        }
+
+        [HttpGet("/broadcaster")]
+        // Overrides the controller scope authentication settings and restricts it to broadcaster only
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "requiresBroadcasterPermission")]
+        public ActionResult<string> OnlyForBroadcastersEyes()
+        {
+            return "Hello World! Welcome to the broadcaster endpoint";
+        }
+
+        [HttpGet("/moderator")]
+        // Overrides the controller scope authentication settings and restricts it to moderator and broadcaster only
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "requiresModeratorPermission")]
+        public ActionResult<string> OnlyForAtLeastModeratorEyes()
+        {
+            return "Hello World! Welcome to the moderator endpoint. Or maybe you are the broadcaster?";
+        }
+    }
+}
---
+++ SyzunaPrograms.ExtensionBackendExample/Startup.cs
@@ -0,0 +1,91 @@
+using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SyzunaPrograms.ExtensionBackendExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddCors();
+
+            // Setup of the JWT Bearer authentication
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    // The extension secret is base64 encoded and has to be decoded before using it
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(Configuration["Twitch:ExtensionSecret"]))
+                    };
+                });
+
+            // This step is kinda optional. You do not need to setup policies for role checking and could do it manually in the endpoint executing method
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("requiresBroadcasterPermissions", policy =>
+                {
+                    policy.RequireClaim("role", "broadcaster");
+                });
+
+                options.AddPolicy("requiresModeratorPermissions", policy =>
+                {
+                    policy.RequireClaim("role", "broadcaster", "moderator");
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseHttpsRedirection();
+
+            // Add CORS to the execution pipeline
+            // This can be tweaked and more restrictive than it its in this example. e.g. you could only allow GET, POST and OPTIONS (OPTIONS is required for CORS)
+            // AllowCredentials is required
+            app.UseCors(options =>
+            {
+                options
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .WithOrigins("https://localhost", $"https://{Configuration["Twitch:ExtensionId"]}.ext-twitch.tv");
+            });
+
+            app.UseRouting();
+
+            // Add our previously setup authentication and authorization to the pipeline
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
